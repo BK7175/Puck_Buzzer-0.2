4af49a36b8d626265fd86f990ad4e65f3159e44f
BRIJESH KAPURIYA <71057381+BK7175@users.noreply.github.com>
2025-04-14 13:24:39 +0200

    Update puck_quiz_buzzer-0.2.js

--- apps/puck_quiz_buzzer-0.2.js
+++ apps/puck_quiz_buzzer-0.2.js
@@ -1,8 +1,8 @@
-// Replace with the unique name for each puck
+                                // Replace with the unique name for each puck
 const PUCK_NAME = "Puck A";
 
 function sendBuzz() {
-  let msg = JSON.stringify({ buzz: true, name: PUCK_NAME });
+  let msg = JSON.stringify({ buzz: true, name: PUCK_NAME });   // Replace with the unique name for each puck
   Bluetooth.println(msg);
   LED.set(); // Flash LED
   setTimeout(() => LED.reset(), 200);
@@ -10,6 +10,6 @@ function sendBuzz() {
 
 setWatch(sendBuzz, BTN, { edge: "rising", repeat: true, debounce: 50 });
 
-NRF.setAdvertising({}, { name: PUCK_NAME, connectable: true });
-console.log(PUCK_NAME + " ready to buzz!");
+NRF.setAdvertising({}, { name: PUCK_NAME, connectable: true });      // Replace with the unique name for each puck
+console.log(PUCK_NAME + " ready to buzz!");    // Replace with the unique name for each puck
 
